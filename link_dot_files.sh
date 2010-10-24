ed2afd1e9b8c88dc285765de10a487cdafde3b9d
Adam Tygart <mozes@ksu.edu>
2010-10-24 11:22:23 -0500

    ignore .sh files

--- link_dot_files.sh
+++ link_dot_files.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for x in `ls -a1 | grep -v git | grep -v '\./'`; do ln -snf ~/conf_files/${x} ../; done
+for x in `ls -a1 | grep -v git | grep -v '\./' | grep -v '\.sh'`; do ln -snf ~/conf_files/${x} ../; done
