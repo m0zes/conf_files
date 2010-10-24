5ba3311f577ffa3b51e47b1c973d1c0c1079ed4f
Adam Tygart <mozes@ksu.edu>
2010-10-24 11:25:50 -0500

    quieted the dot files script

--- link_dot_files.sh
+++ link_dot_files.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-for x in `ls -a1 | grep -v git | grep -v '\./' | grep -v '\.sh'`; do ln -s ~/conf_files/${x} ../; done
+for x in `ls -a1 | grep -v git | grep -v '\./' | grep -v '\.sh'`
+do 
+	ln -s ~/conf_files/${x} ../ >/devnull 2>&1
+done
