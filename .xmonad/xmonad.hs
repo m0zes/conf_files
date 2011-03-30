887d213a1f6a6bc3350c557e78ec7cab6d7f23fc
Adam Tygart <adam.tygart@gmail.com>
2011-03-30 13:33:55 -0500

    Adding xmonad configurations

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,25 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/mozes/.xmobarrc"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts $ layoutHook defaultConfig
+        , logHook = dynamicLogWithPP $ xmobarPP
+            { ppOutput = hPutStrLn xmproc
+            , ppTitle = xmobarColor "green" "" . shorten 65
+            }
+        , terminal = "urxvt -bg black -fg white +sb -vb"
+        , borderWidth = 1
+        } `additionalKeys`
+		[ ((mod1Mask,		xK_F10), spawn "amixer set Master +5%")
+		, ((mod1Mask,		xK_F9 ), spawn "amixer set Master -5%")
+		, ((mod1Mask,		xK_F8 ), spawn "amixer set Master toggle")
+		, ((mod1Mask,		xK_F7 ), spawn "xbacklight -inc 12")
+		, ((mod1Mask,		xK_F6 ), spawn "xbacklight -dec 10")
+		]
